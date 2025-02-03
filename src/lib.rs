fde0cb3ee271212690d3d3939800c2c1633d3d3b
MattWaX <mattwexx2929@gmail.com>
2025-02-03 16:40:45 +0100

    "done" feature implemented

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@ pub fn run() -> Result<(), Box<dyn Error>> {
 
     match matches.subcommand() {
         Some(("add", matches)) => add(matches)?,
-        Some(("done", _matches)) => println!("done"),
+        Some(("done", matches)) => done(matches)?,
         Some(("remove", _matches)) => println!("remove"),
         _ => (),
     }
@@ -125,6 +125,36 @@ fn add(matches: &ArgMatches) -> Result<(), Box<dyn Error>> {
             new_content.push_str(item);
             new_content.push('\n');
         }
+    }
+
+    fs::write(get_data_file_path()?, new_content)?;
+
+    Ok(())
+}
+
+fn done(matches: &ArgMatches) -> Result<(), Box<dyn Error>> {
+    let items = args_collect(matches, "ITEM");
+    let content: String = get_data_file_content()?;
+    let mut new_content: String = String::from("");
+    let mut change_flag: bool = false;
+
+    dbg!(&content);
+
+    for line in content.lines() {
+        for item in &items {
+            if line.contains(&("- [ ] ".to_owned() + item).to_owned()) {
+                new_content.push_str("- [x] ");
+                new_content.push_str(item);
+                new_content.push('\n');
+                change_flag = true;
+                break;
+            }
+        }
+        if !change_flag {
+            new_content.push_str(line);
+            new_content.push('\n');
+        }
+        change_flag = false;
     }
 
     fs::write(get_data_file_path()?, new_content)?;
--- tasks.md
+++ tasks.md
@@ -3,7 +3,9 @@ add:
 - [x] aggiungere automaticamente checkbox nel file markdown
  
 done:
-- [ ] da implementare 
+- [x] da implementare 
+- [ ] aggiungere flag --all -a per segnare tutto come fatto
 
 remove:
 - [ ] da implementare 
+- [ ] aggiungere flag --all -a per rimuovere tutto
